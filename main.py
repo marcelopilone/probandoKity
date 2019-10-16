36f2f036017ecccf2e26adffd13d573ef7594939
marcelopilone <mdpilone@gmail.com>
2019-10-16 14:49:03 -0300

    segundo commit fallando

--- main.py
+++ main.py
@@ -12,63 +12,63 @@ class MyGrid(GridLayout):
 
 	
 
-    def __init__(self, **kwargs):
-        super(MyGrid, self).__init__(**kwargs)
-        self.cols = 1
+	def __init__(self, **kwargs):
+		super(MyGrid, self).__init__(**kwargs)
+		self.cols = 1
 
-        self.inside = GridLayout()
-        self.inside.cols = 2
+		self.inside = GridLayout()
+		self.inside.cols = 2
 
-        self.inside.add_widget(Label(text="Nombre: "))
-        self.name = TextInput(multiline=False)
-        self.inside.add_widget(self.name)
+		self.inside.add_widget(Label(text="Nombre: "))
+		self.name = TextInput(multiline=False)
+		self.inside.add_widget(self.name)
 
-        self.inside.add_widget(Label(text="Apellido: "))
-        self.lastName = TextInput(multiline=False)
-        self.inside.add_widget(self.lastName)
+		self.inside.add_widget(Label(text="Apellido: "))
+		self.lastName = TextInput(multiline=False)
+		self.inside.add_widget(self.lastName)
 
-        self.inside.add_widget(Label(text="Edad: "))
-        self.edad = TextInput(multiline=False)
-        self.inside.add_widget(self.edad)
+		self.inside.add_widget(Label(text="Edad: "))
+		self.edad = TextInput(multiline=False)
+		self.inside.add_widget(self.edad)
 
-        self.inside.add_widget(Label(text="Email: "))
-        self.email = TextInput(multiline=False)
-        self.inside.add_widget(self.email)
+		self.inside.add_widget(Label(text="Email: "))
+		self.email = TextInput(multiline=False)
+		self.inside.add_widget(self.email)
 
-        self.add_widget(self.inside)
+		self.add_widget(self.inside)
 
-        self.submit = Button(text="Submit", font_size=40)
-        self.submit.bind(on_press=self.pressed)
-        self.add_widget(self.submit)
-    def show_popup():
-        show = P() # Create a new instance of the P class 
-
-        popupWindow = Popup(title="Popup Window", content=show, size_hint=(None,None),size=(400,400)) 
-        # Create the popup window
-
-        popupWindow.open() # show the popup
+		self.submit = Button(text="Submit", font_size=40)
+		self.submit.bind(on_press=self.pressed)
+		self.add_widget(self.submit)
 
 
-    def pressed(self, instance):
-        name = self.name.text
-        last = self.lastName.text
-        email = self.email.text
-        edad = self.edad.text
-        resultadoEdad = edad.isdigit()
-        if resultadoEdad == False:
-        	print('No ingreso un numero')
-        self.show_popup
+	def invalidForm():
+			pop = Popup(title='Invalid Form',
+      content=Label(text='Please fill in all inputs with valid information.'),
+      size_hint=(None, None), size=(400, 400))
 
-        print("Name:", name, "Last Name:", last, "Email:", email, "Edad:", edad)
-        self.name.text = ""
-        self.lastName.text = ""
-        self.email.text = ""
-        self.edad.text = ""
+			pop.open()
+
+
+	def pressed(self, instance):
+		name = self.name.text
+		last = self.lastName.text
+		email = self.email.text
+		edad = self.edad.text
+		resultadoEdad = edad.isdigit()
+		if resultadoEdad == False:
+			invalidForm()
+
+		print("Name:", name, "Last Name:", last, "Email:", email, "Edad:", edad)
+		self.name.text = ""
+		self.lastName.text = ""
+		self.email.text = ""
+		self.edad.text = ""
 
 class MyApp(App):
-    def build(self):
-        return MyGrid()
+	def build(self):
+		return MyGrid()
 
 
 if __name__ == "__main__":
-    MyApp().run()
+	MyApp().run()
